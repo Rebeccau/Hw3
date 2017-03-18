610116d9d405f33017d1247d101a5f9436962df4
rebecca <ungar.rivka@gmail.com>
2017-03-18 23:39:11 +0200

    complete hw

--- app.js
+++ app.js
@@ -3,22 +3,53 @@
 
   angular.module('NarrowItDownApp', [])
   .controller('NarrowItDownController', NarrowItDownController)
-  .service('MenuSearchService', MenuSearchService);
+  .service('MenuSearchService', MenuSearchService)
+  .directive('foundItems', foundItems);
+
+  function foundItems(){
+    var ddo = {
+        templateUrl: 'foundItems.html',
+        scope: {
+          items: '<',
+          onRemove: '&'
+        },
+        controller: foundItemsDirectiveController,
+        controllerAs: 'list',
+        bindToController: true
+    }
+    return ddo;
+  }
+
+  function foundItemsDirectiveController(){
+  }
 
   NarrowItDownController.$inject = ['MenuSearchService'];
   function NarrowItDownController( MenuSearchService ){
     var menuSearch = this;
 
     menuSearch.getItems = function( searchTerm ){
-      var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
 
-      promise.then(function (response) {
-        //console.log(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
+      if(searchTerm){
+        var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
 
+        promise.then(function (response) {
+          if(response.length){
+            menuSearch.found = response;
+            menuSearch.message = "";
+          }else{
+            menuSearch.message = "nothing was found!";
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+      }else{ //textbox was left empty
+        menuSearch.message = "nothing was found!";
+      }
+    }
+
+    menuSearch.removeItem = function( index ){
+      MenuSearchService.removeItem(index);
     }
 
   }
@@ -26,6 +57,8 @@
   MenuSearchService.$inject = ['$http'];
   function MenuSearchService( $http ){
     var service =this;
+
+    var foundItems = [];
 
     service.getMatchedMenuItems = function(searchTerm){
 
@@ -35,17 +68,20 @@
       })
       .then(function (result) {
           // process result and only keep items that match
-          //var foundItems...
+          foundItems.length = 0;
           for(var i=0; i < result.data.menu_items.length; i++){
             var string = result.data.menu_items[i].description;
             if (string.indexOf(searchTerm) !=-1) {
-              console.log(string);
+              foundItems.push(result.data.menu_items[i]);
             }
           }
           // return processed items
-          return result;
+          return foundItems;
       });
-      //return searchTerm;
+    }
+
+    service.removeItem = function(index){
+      foundItems.splice( index, 1 );
     }
   }
 
